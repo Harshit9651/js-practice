03c556de1bb435a391368435f5d6d983e3cff487
Harshit sharma <programming966051@gmail.com>
2024-09-21 17:05:34 +0530

    located resolve status

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-const promiseObjs = new Promise((resolve,reject)=>{
-    let req =true;
-    if(req){
-        resolve([1,2,3,4,5,67,8,9,10])
-    }else{
-        reject('sorry this is error ')
-    }
-}).then(
-    (value)=>{console.log(value)},
+// const  promiseObjs = new Promise((resolve,reject)=>{
+//     let req =true;
+//     if(req){
+//         resolve([1,2,3,4,5,67,8,9,10])
+//     }else{
+//         reject('sorry this is error ')
+//     }
+// }).then(
+//     (value)=>{console.log(value)},
 
-).catch((error)=>{console.log(error)})
+// ).catch((error)=>{console.log(error)})
+
+
+// const methodchaning = new Promise((resolve,reject)=>{
+//    let num = 20;
+//    resolve(num) 
+// }).then((result)=>{console.log(result)
+//     return result+30;
+// })
+// .then((result)=>{
+// console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+const arr = [2, 4, ];
+
+const printFunction = new Promise((resolve, reject) => {
+    let totalDelay = 0;
+
+    arr.forEach((ele, index) => {
+        totalDelay += ele; 
+
+        setTimeout(() => {
+            printCurrentElement(ele); 
+            totalDelay=0;
+            if (index === arr.length - 1) {
+               
+                reject();
+            }
+        }, totalDelay * 1000); 
+    });
+})
+.then(() => {
+    console.log('Elements printed successfully');
+})
+.catch((error) => {
+    console.log('Error occurred:', error);
+});
+
+function printCurrentElement(curr) {
+    console.log(curr);
+}
