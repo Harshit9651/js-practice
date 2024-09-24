2a429d42f6e588aafc25cad05fb14ff5920be57f
Harshit sharma <programming966051@gmail.com>
2024-09-24 18:31:54 +0530

    add child componenet

---
+++ learnvue/src/components/ChildComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div><h1>{{ name }}</h1>
+    <button v-on:click="getfunction()">click me </button></div>
+</template>
+<script>
+export default{
+    name:'ChildComponent',
+    props:{
+        name:String,
+        getfunction: Function
+    }
+}
+
+</script>
--- learnvue/src/components/NewComponent.vue
+++ learnvue/src/components/NewComponent.vue
@@ -1,48 +1,93 @@
 <template >
 <div>
-    <h1 v-if="show">hii this is true condition </h1> 
-    <h1 v-else>hii this is false condition </h1> 
-  
-  <button v-on:click="getdata()">clicl me </button>
-  <button v-on:dblclick="getnewdata('data is add here ')">clik me for conole </button>
-  <input type="text" placeholder="enter email" v-model="Email">
-  <br><br>
-  <input type="password" placeholder="enter password" v-model="password">
-<br>
-<button v-on:click="getformdata()"> Get Form data </button>
-
-<button v-on:click="show=!show"> beti chod </button>
+<ul>
+    <li v-for="item in learn" :key ="item">
+        {{ item }}
+    </li>
+</ul>
+<br><br>
+<ul>
+    <li v-for="data in users" :key ="data">
+ name :{{  data.name }}  email: {{ email = data.email }}  phoneNumber:{{ phonenumber = data.number }}
+    </li>
+</ul>
+<ChildComponent name="harshit sharma " :getfunction ='getfunction'/>
 </div>
 </template>
 <script>
-
+import ChildComponent from './ChildComponent.vue';
 export default{
+    
     name:'NewComponent',
+    components:{
+ChildComponent
+    },methods:{
+        getfunction(){
+console.warn('called a function')
+alert('hello')
+        }
+    },
     data(){
        return {
-     
-    Email:null,
-    password:null,
-    show:false,
-        
+        name:'bhandu lala',
+learn:['ram', 'nam' , 'staye ho', 'hari', 'ko', 'shri', 'hari','kaho', 'bolo'],
+ users: [
+  {
+    name: "Harshit Sharma",
+    email: "harshit.sharma@example.com",
+    number: "9876543210"
+  },
+  {
+    name: "Anjali Mehta",
+    email: "anjali.mehta@example.com",
+    number: "9123456789"
+  },
+  {
+    name: "Rajesh Kumar",
+    email: "rajesh.kumar@example.com",
+    number: "9234567890"
+  },
+  {
+    name: "Pooja Singh",
+    email: "pooja.singh@example.com",
+    number: "9345678901"
+  },
+  {
+    name: "Aman Verma",
+    email: "aman.verma@example.com",
+    number: "9456789012"
+  },
+  {
+    name: "Riya Patel",
+    email: "riya.patel@example.com",
+    number: "9567890123"
+  },
+  {
+    name: "Vikas Yadav",
+    email: "vikas.yadav@example.com",
+    number: "9678901234"
+  },
+  {
+    name: "Sneha Gupta",
+    email: "sneha.gupta@example.com",
+    number: "9789012345"
+  },
+  {
+    name: "Mohit Sharma",
+    email: "mohit.sharma@example.com",
+    number: "9890123456"
+  },
+  {
+    name: "Priya Agarwal",
+    email: "priya.agarwal@example.com",
+    number: "9901234567"
+  }
+]
+
+
+  
         }
-    },methods:{
-        getdata(){
-           
-           return this.count = 1+this.count
-     
-        },
-        getnewdata(){
-           
-         alert(`bhn ke lode teri ma ki chut sale ${this.count}`)
-            
-        },
-        getformdata(){
-            console.log(`user enail is ${this.Email} and User Password is ${this.password}`)
-        },
-    
         
     }
-    
 }
 </script>
