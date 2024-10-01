c36fccc72e95f0ab2223838921d4c00c042137c4
Harshit sharma <programming966051@gmail.com>
2024-10-01 20:09:43 +0530

    add images in pinia store

--- hompage/src/stores/counter.js
+++ hompage/src/stores/counter.js
@@ -1,18 +1,53 @@
-import { defineStore } from "pinia";
 
-export const piniaStore = defineStore('counter',{
-    state:()=>({
-        count:1
-    }),
-    getters:{
-        doubblecount :state=>state.count*2
+import { defineStore } from 'pinia';
+
+export const useImageStore = defineStore('imageStore', {
+
+  state: () => ({
+    images: [
+      {
+        label: "Bedroom",
+        src: "https://images.pexels.com/photos/279746/pexels-photo-279746.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        label: "Kitchen",
+        src: "https://images.pexels.com/photos/2724749/pexels-photo-2724749.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        label: "Bathroom",
+        src: "https://images.pexels.com/photos/1910472/pexels-photo-1910472.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+    
+    ],
+    activeIndex: null,
+  }),
+
+
+  getters: {
+ 
+    activeImage(state) {
+      return state.activeIndex !== null ? state.images[state.activeIndex] : null;
     },
-    actions:{
-        increment(){
-            this.count++
-        }
-    }
-})
+
+    totalImages(state) {
+      return state.images.length;
+    },
+  },
+
+ 
+  actions: {
+   
+    selectImage(index) {
+      if (index >= 0 && index < this.images.length) {
+        this.activeIndex = index;
+      }
+    },
+
+    addImage(label, src) {
+      this.images.push({ label, src });
+    },
+  },
+});
 
 // we dont use ref in pinia store
 // state is like data
