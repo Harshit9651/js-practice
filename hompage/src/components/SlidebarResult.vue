7676670251fe14926b11aa97e561c9473cf0c0a9
Harshit sharma <programming966051@gmail.com>
2024-10-02 22:51:34 +0530

    add some data set and complate the componenet

--- hompage/src/components/SlidebarResult.vue
+++ hompage/src/components/SlidebarResult.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="main">
     <div class="sidebar">
-      <div class="header"><h2>Output Settings</h2></div>
-      <div class="br-tag"><hr /></div>
+      <div class="header">
+        <h2>Output Settings</h2>
+      </div>
+      <div class="br-tag">
+        <hr />
+      </div>
       <div class="text-input">
         <label class="input-label" for="">Text Input</label>
         <input class="input-input" type="text" />
       </div>
-      <div class="br-tag"><hr /></div>
+      <div class="br-tag">
+        <hr />
+      </div>
       <div class="styles">
-        <h3>styles</h3>
-        <div class="circle"><i class="fa-solid fa-plus"></i></div>
+        <h3>Styles</h3>
+        <div class="circle" @click="toggleCards">
+          <i :class="showCards ? 'fa-solid fa-minus' : 'fa-solid fa-plus'"></i>
+        </div>
+      </div>
+
+      <div v-if="showCards" class="cards">
+        <div class="card" v-for="(card, index) in cards" :key="index">
+          <img :src="card.image" alt="Image" class="card-image" />
+        </div>
+      </div>
+      <div class="content">
+        <h4>Insperitation Image</h4>
+        <p>Lorem ipsum dolor, sit amet consectetur.</p>
+      </div>
+      <div class="add-data">
+        <div class="add-data-circle"><i class="fa-solid fa-plus"></i></div>
+        Upload Insperitation Image
       </div>
     </div>
   </div>
@@ -19,20 +41,41 @@
 <script setup>
 import { ref } from "vue";
 
-// State to toggle the visibility of cards
 const showCards = ref(false);
 
-// Sample card data with images
 const cards = ref([
-  { title: "Card 1", image: "https://via.placeholder.com/150" },
-  { title: "Card 2", image: "https://via.placeholder.com/150" },
-  { title: "Card 3", image: "https://via.placeholder.com/150" },
-  { title: "Card 4", image: "https://via.placeholder.com/150" },
-  { title: "Card 5", image: "https://via.placeholder.com/150" },
-  { title: "Card 6", image: "https://via.placeholder.com/150" }
+  {
+    title: "Card 1",
+    image:
+      "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    title: "Card 2",
+    image:
+      "https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    title: "Card 3",
+    image:
+      "https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    title: "Card 4",
+    image:
+      "https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    title: "Card 5",
+    image:
+      "https://images.pexels.com/photos/1648771/pexels-photo-1648771.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    title: "Card 6",
+    image:
+      "https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
 ]);
 
-// Function to toggle the display of cards and change the icon
 function toggleCards() {
   showCards.value = !showCards.value;
 }
@@ -90,11 +133,77 @@ function toggleCards() {
       border: none;
     }
   }
-  .styles{
-    display:flex;
+  .styles {
+    display: flex;
     justify-content: space-between;
-    background-color: aqua;
+    background-color: rgb(255, 255, 255);
+    padding: 10px;
+  }
+  .circle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    background: #d2cecc;
+    color: white;
+    cursor: pointer;
+  }
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    margin-top: 20px;
+    justify-content: space-around;
+    .card {
+      background-color: #ffffff;
 
+      width: 90px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      border-radius: 8px;
+    }
+    .card-image {
+      height: 90px;
+      width: 90px;
+      border-radius: 10px;
+    }
+  }
+  .content {
+    margin-top: 7%;
+    margin-left: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+  .add-data {
+    margin-top: 8%;
+    margin-left: 20px;
+    height: 56px;
+    width: 294px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid;
+    border-image-source: linear-gradient(277.64deg, #4D8AFF 8.32%, #F53F9E 51.79%, #FF9051 96.13%);
+    border-image-slice: 1;
+    gap:10px;
+    color: #ff5c00;
+    border-radius: 50%;
+
+    .add-data-circle {
+      height: 35px;
+      width: 35px;
+      background: #ff5c00;
+      color: white;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
 }
 </style>
