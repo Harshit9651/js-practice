9ebdeca47f65d0b76faa824dfb67922082107c07
Harshit sharma <programming966051@gmail.com>
2024-10-05 21:41:07 +0530

    add some data

--- hompage/src/components/CustimizePage.vue
+++ hompage/src/components/CustimizePage.vue
@@ -23,6 +23,8 @@ const cardData = [
   { iconClass: "fa-solid fa-ribbon", text: "Change Wall/floor", id: 4 },
   { iconClass: "fa-solid fa-store", text: "Room styles", id: 5 },
   { iconClass: "fa-solid fa-ban", text: "Remove Objects", id: 6 },
+  { iconClass: "fa-solid fa-ban", text: "Remove Objects", id: 6 },
+
 ];
 const selectCard = ref(null);
 function handelClick(id){
